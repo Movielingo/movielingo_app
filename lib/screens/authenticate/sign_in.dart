1d320a994886efb70c17dba61da147b650ab1a48
Constantin Unterkofler <50672977+conuko@users.noreply.github.com>
2023-11-14 10:38:37 +0100

    feat(sign_in): set up sign in form

--- lib/screens/authenticate/sign_in.dart
+++ lib/screens/authenticate/sign_in.dart
@@ -14,28 +14,48 @@ class _SignInState extends State<SignIn> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.greenAccent[100],
+      backgroundColor: Colors.black,
       appBar: AppBar(
         backgroundColor: Colors.greenAccent[400],
         elevation: 0.0,
         title: const Text('Sign in to MovieLingo'),
       ),
       body: Container(
-        padding: const EdgeInsets.symmetric(vertical: 20.0, horizontal: 50.0),
-        child: ElevatedButton(
-          child: const Text('Sign in anonymously'),
-          onPressed: () async {
-            // sign in anonymously
-            dynamic result = await _auth.signInAnon();
-            if (result == null) {
-              print('error signing in');
-            } else {
-              print('signed in');
-              print(result.uid);
-            }
-          },
-        ),
-      ),
+          padding: const EdgeInsets.symmetric(vertical: 20.0, horizontal: 50.0),
+          child: Form(
+            child: Column(children: <Widget>[
+              const SizedBox(height: 20.0),
+              TextFormField(
+                  style: const TextStyle(color: Colors.white),
+                  onChanged: (val) {
+                    // setState(() => email = val);
+                  }),
+              const SizedBox(height: 20.0),
+              TextFormField(
+                style: const TextStyle(color: Colors.white),
+                obscureText: true,
+                onChanged: (val) {
+                  // setState(() => password = val);
+                },
+              ),
+              const SizedBox(height: 20.0),
+              ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.greenAccent[400],
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(0),
+                    ),
+                  ),
+                  onPressed: () async {
+                    // print(email);
+                    // print(password);
+                  },
+                  child: const Text(
+                    'Sign in',
+                    style: TextStyle(color: Colors.white),
+                  )),
+            ]),
+          )),
     );
   }
 }
