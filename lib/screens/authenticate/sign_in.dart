ac07a4e1cd971ccd350698853f69736285cb1172
Constantin Unterkofler <50672977+conuko@users.noreply.github.com>
2023-11-14 15:06:54 +0100

    feat(*): add login with email process
    - adds the functionality to login in with email and password
    - connected to firebase
    - when a user exists and the password is correct you can login / otherwise --> error

--- lib/screens/authenticate/sign_in.dart
+++ lib/screens/authenticate/sign_in.dart
@@ -12,10 +12,12 @@ class SignIn extends StatefulWidget {
 
 class _SignInState extends State<SignIn> {
   final AuthService _auth = AuthService();
+  final _formKey = GlobalKey<FormState>();
 
   // text field state
   String email = '';
   String password = '';
+  String error = '';
 
   @override
   Widget build(BuildContext context) {
@@ -37,15 +39,19 @@ class _SignInState extends State<SignIn> {
       body: Container(
           padding: const EdgeInsets.symmetric(vertical: 20.0, horizontal: 50.0),
           child: Form(
+            key: _formKey,
             child: Column(children: <Widget>[
               const SizedBox(height: 20.0),
               TextFormField(
+                  validator: (val) => val!.isEmpty ? 'Enter an email' : null,
                   style: const TextStyle(color: Colors.white),
                   onChanged: (val) {
                     setState(() => email = val);
                   }),
               const SizedBox(height: 20.0),
               TextFormField(
+                validator: (val) =>
+                    val!.length < 6 ? 'Enter a password 6+ chars long' : null,
                 style: const TextStyle(color: Colors.white),
                 obscureText: true,
                 onChanged: (val) {
@@ -61,13 +67,24 @@ class _SignInState extends State<SignIn> {
                     ),
                   ),
                   onPressed: () async {
-                    print(email);
-                    print(password);
+                    if (_formKey.currentState!.validate()) {
+                      dynamic result = await _auth.signInWithEmailAndPassword(
+                          email, password);
+                      if (result == null) {
+                        setState(() =>
+                            error = 'Could not sign in with those credentials');
+                      }
+                    }
                   },
                   child: const Text(
                     'Sign in',
                     style: TextStyle(color: Colors.white),
                   )),
+              const SizedBox(height: 12.0),
+              Text(
+                error,
+                style: const TextStyle(color: Colors.red, fontSize: 14.0),
+              )
             ]),
           )),
     );
--- lib/services/auth.dart
+++ lib/services/auth.dart
@@ -27,6 +27,17 @@ class AuthService {
   }
 
   // sign in with email & password
+  Future signInWithEmailAndPassword(String email, String password) async {
+    try {
+      UserCredential result = await _auth.signInWithEmailAndPassword(
+          email: email.trim(), password: password.trim());
+      User? user = result.user;
+      return _userfromFirebase(user!);
+    } catch (e) {
+      print(e.toString());
+      return null;
+    }
+  }
 
   // register with email & password
   Future registerWithEmailAndPassword(String email, String password) async {
