f65feec59c07da1cfb9ba71e126bf1bc62121609
Constantin Unterkofler <50672977+conuko@users.noreply.github.com>
2023-11-15 11:52:12 +0100

    feat(auth): add minor code clean up

--- lib/services/auth.dart
+++ lib/services/auth.dart
@@ -9,7 +9,8 @@ class AuthService {
     return user != null ? MyUser(uid: user.uid) : null;
   }
 
-  // auth change user stream that returns back a user whenever there is a change in authentication
+  // auth change user stream that returns back a user whenever there is a
+  // change in authentication
   Stream<MyUser?> get user {
     return _auth.authStateChanges().map(_userfromFirebase);
   }
@@ -53,7 +54,6 @@ class AuthService {
   }
 
   // sign out
-
   Future signOut() async {
     try {
       return await _auth.signOut();
