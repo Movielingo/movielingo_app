1cd595a8eb2c6476513bd9454c54de02bce0b887
Constantin Unterkofler <50672977+conuko@users.noreply.github.com>
2023-11-30 14:56:51 +0100

    feat(models): create user episode&movie classes

---
+++ lib/models/user_episode.dart
@@ -0,0 +1,17 @@
+class UserEpisode {
+  final String mediaId;
+  final String title;
+  final int progress;
+  final String translationLanguage;
+  final String mediaLanguage;
+  final String image;
+
+  UserEpisode({
+    required this.mediaId,
+    required this.title,
+    required this.progress,
+    required this.translationLanguage,
+    required this.mediaLanguage,
+    required this.image,
+  });
+}
---
+++ lib/models/user_movie.dart
@@ -0,0 +1,17 @@
+class UserMovie {
+  final String mediaId;
+  final String title;
+  final int progress;
+  final String translationLanguage;
+  final String mediaLanguage;
+  final String image;
+
+  UserMovie({
+    required this.mediaId,
+    required this.title,
+    required this.progress,
+    required this.translationLanguage,
+    required this.mediaLanguage,
+    required this.image,
+  });
+}
