24560ab9ae8b191fae0d1cb8a8af5503e94a636f
Constantin Unterkofler <50672977+conuko@users.noreply.github.com>
2024-07-26 12:03:20 +0200

    feat(media_detail): display all necessary data

--- lib/screens/media_detail.dart
+++ lib/screens/media_detail.dart
@@ -14,9 +14,7 @@ class MediaDetail extends StatelessWidget {
         Get.find<FirebaseStorageService>();
 
     return Scaffold(
-      appBar: AppBar(
-        title: Text(movie.title),
-      ),
+      appBar: AppBar(),
       body: FutureBuilder<String?>(
         future: firebaseStorageService.getImage(movie.imgRef),
         builder: (context, snapshot) {
@@ -25,23 +23,54 @@ class MediaDetail extends StatelessWidget {
           } else if (snapshot.hasError || !snapshot.hasData) {
             return const Center(child: Text('Error loading image'));
           } else {
-            return Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Image.network(
-                  snapshot.data!,
-                  fit: BoxFit.cover,
-                ),
-                const SizedBox(height: 8),
-                Text(
-                  movie.title,
-                  style: const TextStyle(
-                      fontSize: 24, fontWeight: FontWeight.bold),
-                ),
-                const SizedBox(height: 8),
-                Text(
-                    movie.description), // Assuming you have a description field
-              ],
+            return SingleChildScrollView(
+              padding: const EdgeInsets.all(16),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Image.network(
+                    snapshot.data!,
+                    fit: BoxFit.cover,
+                  ),
+                  const SizedBox(height: 8),
+                  Text(
+                    movie.title,
+                    style: const TextStyle(
+                        fontSize: 24, fontWeight: FontWeight.bold),
+                  ),
+                  const SizedBox(height: 8),
+                  Text('Directed by ${movie.director}'),
+                  const SizedBox(height: 4),
+                  Text((movie.release).toString()),
+                  const SizedBox(height: 4),
+                  Text('${movie.lengthMin.toString()} minutes'),
+                  const SizedBox(height: 16),
+                  Text(movie.description),
+                  const SizedBox(height: 16),
+                  const Text('Original language: ENGLISH'),
+                  const SizedBox(height: 4),
+                  Text(
+                      'Available translation languages to study: ${movie.translationLanguage.join('').toUpperCase()}'),
+                  const SizedBox(height: 16),
+                  Text(
+                      '${movie.vocabCounts.a1VocabCount.toString()} A1 vocabulary words'),
+                  const SizedBox(height: 4),
+                  Text(
+                      '${movie.vocabCounts.a2VocabCount.toString()} A2 vocabulary words'),
+                  const SizedBox(height: 4),
+                  Text(
+                      '${movie.vocabCounts.b1VocabCount.toString()} B1 vocabulary words'),
+                  const SizedBox(height: 4),
+                  Text(
+                      '${movie.vocabCounts.b2VocabCount.toString()} B2 vocabulary words'),
+                  const SizedBox(height: 4),
+                  Text(
+                      '${movie.vocabCounts.c1VocabCount.toString()} C1 vocabulary words'),
+                  const SizedBox(height: 4),
+                  Text(
+                      '${movie.vocabCounts.c2VocabCount.toString()} C2 vocabulary words'),
+                ],
+              ),
             );
           }
         },
